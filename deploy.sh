0f6d4ec796b24689c89a0486b4314164027cb400
Tyler Ayers <tylerayers@carbon7.localdomain>
2021-12-17 09:38:59 +0100

    removed deletion of products because it often fails because apps exist

--- deploy.sh
+++ deploy.sh
@@ -11,9 +11,9 @@ NEWREV="${NEWREV#\"}"
 apigeecli apis deploy -n data-ai-services -e test1 -s tyservice2@bruno-1407a.iam.gserviceaccount.com -v $NEWREV -r -o bruno-1407a -t $TOKEN
 
 # Delete any previously deployed products
-apigeecli products delete -n "Image API" -o bruno-1407a -t $TOKEN
-apigeecli products delete -n "Text Analytics API" -o bruno-1407a -t $TOKEN
-apigeecli products delete -n "Translation API" -o bruno-1407a -t $TOKEN
+# apigeecli products delete -n "Image API" -o bruno-1407a -t $TOKEN
+# apigeecli products delete -n "Text Analytics API" -o bruno-1407a -t $TOKEN
+# apigeecli products delete -n "Translation API" -o bruno-1407a -t $TOKEN
 
 # Import API products
 apigeecli products import -f ./products/ai-products.json -o bruno-1407a -t $TOKEN
